3b1324fe0623d53107b0eb5d2b78af8d29c1ddd7
danillotc <danillotc@hotmail.com>
2020-07-25 00:19:02 -0300

    Updates

---
+++ precache-manifest.6f625b3a994b97944042d5a843503836.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55c2c3cc57c4988951626553cacafcc9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "12a6563eda994005ecab",
+    "url": "/static/css/main.adc2ef16.chunk.css"
+  },
+  {
+    "revision": "82853dc818081a65c7d7",
+    "url": "/static/js/2.8fca5919.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8fca5919.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12a6563eda994005ecab",
+    "url": "/static/js/main.8479b221.chunk.js"
+  },
+  {
+    "revision": "ea3972fd8a25792d4dbb",
+    "url": "/static/js/runtime-main.9305eb6c.js"
+  }
+]);
